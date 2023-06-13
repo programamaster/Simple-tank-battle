64dc43e3d30d8a42a3abee47088f6dcfd514aa3d
noobprogrammer <129837722+programamaster@users.noreply.github.com>
2023-06-14 00:13:29 +0800

    Add files via upload

---
+++ 01-基础知识-类型转换/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_基础知识_类型转换
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
